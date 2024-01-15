e808bef56a1432bb0dc5d6f9b6442d794ca8be8e
Nathan Geronimo <nathangero@yahoo.com>
2024-01-14 23:49:34 -0800

    add more questions/answers

--- assets/js/answers.js
+++ assets/js/answers.js
@@ -3,8 +3,13 @@ import { ROUND1_QUESTIONS, ROUND2_QUESTIONS, ROUND3_QUESTIONS } from "./question
 export const ROUND1_ANSWERS = {
   WHO_ARE_YOU: ["emily", "baby", "smol baby", "smol kitty cat baby", "smol kitty cat puzzle piece baby"],
   FAV_FLOWER: ["poppy", "poppies", "rose", "roses"],
+  FAV_PIZZA_PLACE: ["vesta"],
+  WHO_LOVES_YOU: ["nathan", "nate", "big baby", "big tootin baby", "big tooting baby", "baby"]
 }
 
 export const ROUND2_ANSWERS = {
   FAV_PERSON: ["nathan", "baby", "big baby", "abbie", "jenna", "family", "jarmans", "caroline", "harlows", "harlow", "riley", "alexa", "reese", "alison", "garron", "bishop", "cringe content crew", "ccc"],
+  FAV_PIZZA: ["sausage and honey", "sausage & honey", "sausage", "honey", "mozzarella", "rosemary potato and bacon", "potato", "bacon", "rosemary", "cherry"],
+  STARDEW_ACTIVITY: ["fishing", "fish", "fighting", "fight", "mining", "mine", "putting sprinklers"],
+  FAV_CHRISTMAS_GIFT: ["deskmat", "desk mat", "fish desk mat", "fishy deskmat", "fishy desk mat", "fish deskmat", "mouse pad", "mousepad", "fish mouse pad", "fish mousepad"]
 }
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,14 +1,18 @@
 export const ROUND1_QUESTIONS = {
   WHO_ARE_YOU: "Who are you?",
   FAV_FLOWER: "What's your favorite flower?",
+  FAV_PIZZA_PLACE: "What's your favorite pizza place we frequent?",
+  WHO_LOVES_YOU: "Who loves you soooooo much no matter what?",
 }
 
 export const ROUND2_QUESTIONS = {
   FAV_PERSON: "Who's someone you like being around?",
+  FAV_PIZZA: "What's your favorite pizza or topping at Vesta?",
+  STARDEW_ACTIVITY: "What's a favorite Stardew activity big baby likes to do?",
+  FAV_CHRISTMAS_GIFT: "What did you give big baby that he loves so much?",
 }
 
 export const ROUND3_QUESTIONS = {
   FAV_MEMORY: "What's one of your favorite memories of last year?",
   ACCOMPLISHMENT: "What's something you accomplished last year?",
-  
 }
