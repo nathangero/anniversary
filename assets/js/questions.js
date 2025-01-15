a00967387a1f7b6a4e14005b1b483c8eb31c6a93
Nathan Geronimo <nathangero@yahoo.com>
2025-01-15 00:12:44 -0800

    add new video for 3rd year anniversary

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -10,7 +10,7 @@ export const ROUND2_QUESTIONS = {
   FAV_PERSON: "Who's someone you like being around?",
   FAV_PIZZA: "What's your favorite pizza or topping at Vesta?",
   STARDEW_ACTIVITY_BIG: "What's a favorite Stardew activity big baby likes to do?",
-  STARDEW_ACTIVITY_SMOL: "What's a favorite Stardew activity smole baby likes to do?",
+  STARDEW_ACTIVITY_SMOL: "What's a favorite Stardew activity smol baby likes to do?",
   FAV_CHRISTMAS_GIFT: "What did you give big baby that he loves so much?",
   UTAH_24_HRS: "Where did we go and come back within 24 hours?",
 }
--- assets/js/script.js
+++ assets/js/script.js
@@ -35,7 +35,7 @@ const renderAnniversary = () => {
       <h2>Here's to another year with the best partner ever!!</h2>
       <p>I love you so much baby 😚 and I love eeeeevery second being with you! Here's a recap of being with you in ${year - 1}.</p>
       <br>
-      <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/JSgyVs4GHtI?si=scXprVeMwSjf1Mt5" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/QQq0jmlg2IM?si=PNLeoa8ouAFqsNrr" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
     </section>`
   )
 }
